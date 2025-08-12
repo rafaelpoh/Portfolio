f73ec3076ee58f2b0b6a326383ecd1c8f5754e6f
Rafael Perroni <128108801+rafaelpoh@users.noreply.github.com>
2025-08-12 00:15:33 -0300

    Repaginando o Portfolio

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const container = document.querySelector('.apresentacao__imagem-container');
+const canvas = document.getElementById('meuCanvas');
+if (canvas && container) {
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.src = './icones/fotoperfil2.jpeg';
+
+    container.addEventListener('mouseenter', () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    });
+
+    container.addEventListener('mouseleave', () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
+}
